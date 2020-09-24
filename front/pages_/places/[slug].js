b02b07120877afd5337f87cd95c44f30356cae49
Konrad Rosa <40122362+kerekatu@users.noreply.github.com>
2020-09-24 14:04:10 +0200

    NEW: Trip Place Page

--- front/pages_/places/[slug].js
+++ front/pages_/places/[slug].js
@@ -1,6 +1,7 @@
 import styles from '@/styles/Place.module.scss'
 import PropTypes from 'prop-types'
 import { useRouter } from 'next/router'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 import { getDataWithSlug, getAllData } from '@/lib/api'
 import markdownToHtml from '@/lib/markdownToHtml'
@@ -8,9 +9,12 @@ import markdownToHtml from '@/lib/markdownToHtml'
 import Layout from '@/components/Layout/Layout'
 import { Slider, SliderItem } from '@/components/Slider/Slider'
 import Loading from '@/components/Loading/Loading'
+import { Button } from '@/components/Button/Button'
+import useTranslation from 'next-translate/useTranslation'
 
 const Place = ({ post }) => {
   const router = useRouter()
+  const { t } = useTranslation()
 
   if (!router.isFallback && !post?.slug) {
     return (
@@ -26,7 +30,14 @@ const Place = ({ post }) => {
         <Loading />
       ) : (
         <section className={styles.place}>
-          <h2>{post.title}</h2>
+          <div className={styles.place_top}>
+            <Button
+              type="secondary"
+              icon={faArrowLeft}
+              onClick={() => router.push('/trips')}
+            />
+            <h2>{post.title}</h2>
+          </div>
           {post.media[0] && (
             <div className={styles.place__slider}>
               <Slider showAmount>
@@ -40,6 +51,29 @@ const Place = ({ post }) => {
             </div>
           )}
           <div className={styles.place__content}>
+            <div className={styles.place__info}>
+              <p>
+                <strong>{t('common:trip-address')}:</strong> {post.address}
+              </p>
+              <p>
+                <strong>{t('common:trip-depth')}:</strong> {post.depth} m
+              </p>
+              <p>
+                <strong>{t('common:trip-difficulty')}:</strong>{' '}
+                {post.trip_category.category}
+              </p>
+              <p>
+                <strong>GPS: </strong>
+                <a
+                  href={`https://www.google.com/maps/@${post.map_marker.lat},${post.map_marker.lng},18z`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline"
+                >
+                  {post.map_marker.lat}, {post.map_marker.lng}
+                </a>
+              </p>
+            </div>
             <div
               className={styles.place__markdown}
               dangerouslySetInnerHTML={{ __html: post.content }}
